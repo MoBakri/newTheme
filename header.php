e3588b79ed893d8a4c0c0529dc9306faf5b6f665
MoBakri <mohamedbinbakri@gmail.com>
2021-07-10 14:20:58 +0200

    new wp theme

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
+<head charset=<?php bloginfo('charset'); ?>>
+<link rel="pingback" href="<?php bloginfo('pingback_url');?>"/>
+<title>
+<?php bloginfo('name')?>
+</title>
+<?php wp_head();?>
+</head>
+<body>
